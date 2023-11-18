904f550f465c141352895584039e4fe2f7076862
wahfl2 <wahflbeljihn@gmail.com>
2023-11-18 13:18:04 -0600

    backend

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-import json, requests
+import time, json, requests
 from clinic_match import ClinicMatch, ClinicQuery
 import pandas as pd
 
@@ -11,6 +11,8 @@ OUT_URL = 'https://6d85-155-92-14-110.ngrok-free.app/input?id='
 
 class Backend:
     def start():
+        print("Starting!")
+        
         LLMFactory.initiate_model(model_name="lmsys/vicuna-7b-v1.3")
         key = read_api_key()
 
@@ -18,10 +20,16 @@ class Backend:
         cq = ClinicQuery(key)
 
         while True:
-           Backend.loop()
+            start = time.time()
+            Backend.loop()
+            elapsed = time.time() - start
+            
+            if elapsed < 2.0:
+                time.sleep(2.0 - elapsed)
             
     def loop():
         site = requests.get(WATCH_URL)
+        print(site.text)
         o = json.loads(site.text)
         for key, conversation in o['conversations'].items():
             if conversation.__len__() % 2 == 1:
@@ -40,5 +48,3 @@ class Backend:
                 
         out_json = json.dumps(out_object)
         requests.post(OUT_URL + key, json=out_json)
-
-Backend.start()
